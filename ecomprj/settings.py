2b54360ec210d75cf76c065952ccda4d3b9a92d8
Umair Choudhary <82431716+umair-choudhary94@users.noreply.github.com>
2024-05-18 06:51:53 +0500

    Update settings.py

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "p-z9m(q24fsa)a$9&4r$pf3$z@&4og82ih&zdffzx05)lt$fp8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
